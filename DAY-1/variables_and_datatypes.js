82134bc9e3dde13f5e27c844ae76935422447f39
suryakanta007 <sahusuryakanta073@gmail.com>
2024-07-13 20:59:12 +0530

    complte day1 task

---
+++ DAY-1/variables_and_datatypes.js
@@ -0,0 +1,45 @@
+// activitie-1
+// variable declaration using var and let and assign number to var and string to let.
+// var num = 7;
+// let mom = "LOVE";
+// console.log(num,"\n",mom);
+
+//activitie-2
+//variable declaration  using const and assing to a boolean value to it.
+// 
+
+//activitie-3
+//create variables of different types and log it types using typeof operators
+// let fav = 7;
+// let mentor = "Dhoni";
+// let isLoveToCode = true;
+// let goodLife = ["Health","Wealth","Relationship"];
+// let arr = [2,3,4,5];
+// let obj = {
+//     myName: "Surya",
+//     age:18,
+// }
+// console.log(typeof fav);
+// console.log(typeof mentor);
+// console.log(typeof isLoveToCode);
+// console.log(typeof goodLife);
+// console.log(typeof obj);
+
+//in this actvite the array also give object and i will know every thing in javaScript is object.
+
+//activitie 4:
+// Reassigning variable using let
+let chai = "Desi";
+console.log(chai);
+chai  = "chai aur code";
+console.log(chai);
+
+//activitie 5:
+//reassigning variable using const
+const num = 10;
+console.log(num);
+num = 17;
+console.log(num);
+//give error const variable can't be reassign.
+
+
